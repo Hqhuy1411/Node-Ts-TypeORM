22d177b6e8b4117d206f50b3aed0688497cf183a
Huyyyyyyyyyyyyyyyyyy <hdqhuy.1411@gmail.com>
2023-04-09 23:29:50 +0700

    CRUD book

--- src/app/controllers/BookController.ts
+++ src/app/controllers/BookController.ts
@@ -5,12 +5,14 @@ import { Paginator } from "../../database/Pagination";
 import { CreateAuthDto, UpdateAuthDto } from "../dtos/CreateAuthDTO";
 import { validate } from "class-validator"
 import { Book } from "../../database/entities/Books";
+import { Users } from "../../database/entities/Users";
+import { Author } from "../../database/entities/Authors";
 
 
 export class BookController {
 
-    async getAll(req: Request, res: Response):Promise<Response> {
-        const builder =  AppDataSource.getRepository(Book).createQueryBuilder('books').leftJoinAndSelect('books.author', 'authors');
+    async getAll(req: Request, res: Response): Promise<Response> {
+        const builder = AppDataSource.getRepository(Book).createQueryBuilder('books').leftJoinAndSelect('books.author', 'authors');
         const { records: books, pagination } = await Paginator.paginate(builder, req)
 
         return ResponseUtil.sendResponse<Book[]>(res, "Success", books, pagination)
@@ -28,15 +30,79 @@ export class BookController {
 
     }
 
-    async create(req: Request, res: Response)  {
-         
+    async create(req: any, res: Response) {
+        const { idUser } = req.user
+        const data = req.body
+
+        const repoUser = AppDataSource.manager.getRepository(Users)
+        const repoAuthor = AppDataSource.getRepository(Author)
+        const repoBook = AppDataSource.getRepository(Book)
+
+        const user = await repoUser.findOne({
+            where : {
+                id : idUser
+            },
+            relations : ['author']
+        })
+        const author = await repoAuthor.findOneByOrFail({id : user!.author.id})
+        
+        const book = repoBook.create({...data,author})
+        const bookSave = await repoBook.save(book)
+
+        return ResponseUtil.sendResponse(res, "Save success", bookSave)
     }
 
-    async update(req: Request, res: Response)  {
-   
+    async update(req: any, res: Response) {
+        const {idBook} = req.params
+        const data = req.body
+        const { idUser } = req.user
+
+
+        const repoUser = AppDataSource.manager.getRepository(Users)
+        const repoAuthor = AppDataSource.getRepository(Author)
+        const repoBook = AppDataSource.getRepository(Book)
+
+        const user = await repoUser.findOne({
+            where : {
+                id : idUser
+            },
+            relations : ['author']
+        })
+        const author = await repoAuthor.findOneByOrFail({id : user!.author.id})
+        const book = await repoBook.findOneByOrFail({id : idBook})
+
+        if(book.author.id == author.id){
+            repoBook.merge(book,data)
+            await repoBook.save(book)
+            return ResponseUtil.sendResponse(res, "Update success", book)
+        }else{
+            return ResponseUtil.sendResponse(res, "You are not author of book", null)
+        }
     }
 
-    async delete(req: Request, res: Response) {
+    async delete(req: any, res: Response) {
+        const {idBook} = req.params
+        const { idUser } = req.user
 
+        const repoUser = AppDataSource.manager.getRepository(Users)
+        const repoAuthor = AppDataSource.getRepository(Author)
+        const repoBook = AppDataSource.getRepository(Book)
+
+        const user = await repoUser.findOne({
+            where : {
+                id : idUser
+            },
+            relations : ['author']
+        })
+        const author = await repoAuthor.findOneByOrFail({id : user!.author.id})
+        const book = await repoBook.findOneByOrFail({id: idBook})
+        
+        if(book.author.id === author.id){
+            await repoBook.delete(book.id);
+            return ResponseUtil.sendResponse(res, "Delete success", null)
+    
+        }else{
+            return ResponseUtil.sendResponse(res, "You are not author of book", null)
+        }
     }
 }
--- src/routes/book.routers.ts
+++ src/routes/book.routers.ts
@@ -1,16 +1,17 @@
 import {ErrorHandler} from "../app/middlewares/HandlerError"
 import { BookController } from "./..//app/controllers/BookController";
 import  express  from "express";
+import verifyUser from "../app/middlewares/AuthMiddleware";
 
 const router = express.Router();
 const bookController = new BookController;
 
 
-router.get('/' ,bookController.getAll);
+router.get('/' ,ErrorHandler.catchErrors(bookController.getAll));
 router.get('/:id' ,ErrorHandler.catchErrors(bookController.get));
-router.post('/', ErrorHandler.catchErrors(bookController.create));
-router.put('/:id', ErrorHandler.catchErrors(bookController.update));
-router.delete('/:id', ErrorHandler.catchErrors(bookController.delete));
+router.post('/', verifyUser,ErrorHandler.catchErrors(bookController.create));
+router.put('/:idBook', verifyUser,ErrorHandler.catchErrors(bookController.update));
+router.delete('/:idBook', verifyUser,ErrorHandler.catchErrors(bookController.delete));
 
 
 
